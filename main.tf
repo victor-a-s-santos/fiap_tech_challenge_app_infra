40a4406f3dc4bcb3b20498f90a55c1d7cb01d285
Victor Araujo <victor.a.santanna@gmail.com>
2024-10-13 18:02:42 -0300

    adicionando nome randomico do cluster

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "fiap-eks-cluster"
+  cluster_name = "fiap-eks--${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
